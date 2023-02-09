e0a1c2543a71f94f5222780af98e29a027756b84
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-09 12:59:32 +0200

    Check button input if 0

--- script.js
+++ script.js
@@ -6,12 +6,14 @@
 // document.querySelector('.score').textContent = " you suck"
 // document.querySelector('.guess').value = 25 //1 use value for input fields
 
-document.querySelector(".check").addEventListener('click', function(){
+
+document.querySelector(".check").addEventListener('click', function(){ //1 Find the class of .check(button) and add a function to it. When clicked if there is no number in the input, then the if triggers.
     const guess = Number(document.querySelector('.guess').value)
     // console.log(typeof guess);
-    if(!guess) {
+    if(!guess) { //1 if no number is placed in the input(guess) then run this code
         console.log("There is no number");
         document.querySelector('.message').textContent = "⛔️ No number"
     }
 })
+
 
