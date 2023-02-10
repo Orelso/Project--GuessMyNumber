def9ee4e7d75bd1b9b6b38c4ea58f8dfbb2cb33b
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-10 12:46:41 +0200

    restart the game

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1; //1 trunc removes the decimal. numbers between 1-20
-document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 document.querySelector('.check').addEventListener('click', function () { //1 Find the class of .check(button) and add a function to it. When clicked if there is no number in the input, then the if triggers.
@@ -24,8 +23,10 @@ document.querySelector('.check').addEventListener('click', function () { //1 Fin
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'You got it dude 👍';
     document.querySelector('body').style.backgroundColor = 'green'; //1 When the player wins the game, the background color changes
-    document.querySelector('.number').style.width = '70rem';
+    // document.querySelector('.number').style.width = '70rem';
     document.querySelector('.number').textContent = 'GAME OVER! You win🏆';
+    document.querySelector('.number').textContent = secretNumber;
+
     // When the number is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -52,6 +53,25 @@ document.querySelector('.check').addEventListener('click', function () { //1 Fin
     }
   }
 });
+
+function resetGame() {
+    score = 20;
+    // secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = '';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing ...';
+    document.querySelector('.guess').value = null;
+    document.querySelector('body').style.backgroundColor = '';
+    document.querySelector('.number').style.width = '';
+
+  }
+  
+  document.querySelector('.again').addEventListener('click', function () { 
+    resetGame();
+  });
+  
+
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 document.querySelector('.guess').addEventListener('keyup', function (event) {
   if (event.key === 'Enter' || event.key === 'Return') {
